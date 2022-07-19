1bc70f46f6d0520af7a849a9397e4350a4be5081
Ueniry <ueniry.d@escolar.ifrn.edu.br>
2022-07-19 15:14:16 -0300

    Adicionado o Material UI e Text Field

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+import { TextField } from '@mui/material';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+
+
     <div className="App">
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
